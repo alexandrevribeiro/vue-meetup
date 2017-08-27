b2a4aacfe8334a1702c7373dc23e1289ff04486f
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-08-27 15:30:43 -0300

    Removing "router" directive

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <v-list-tile 
           v-for="item in menuItems" 
           :key="item.title"
-          router :to="item.link">
+          :to="item.link">
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -28,7 +28,7 @@
         <v-btn flat 
           v-for="item in menuItems" 
           :key="item.title"
-          router :to="item.link">
+          :to="item.link">
           <v-icon left>{{ item.icon }}</v-icon>
           {{ item.title }}
           </v-btn>
